ed2b854d69f85eb2da8ac10ed56b7bd4293f25d0
Axel <axel.jakobsson3@gmail.com>
2023-05-07 16:55:00 +0200

    Styled a little

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 
-
 <script>
+	import Button from '../lib/Button.svelte';
 	import { onMount } from 'svelte';
 
 	/**
@@ -8,8 +8,11 @@
      */
 	let canvas;
 
-	let maze_color = "#000";
-	let maze_number = 0;
+	let maze_color = "#FFF";
+	let maze_number = 7;
+
+	const width = 704;
+	const height = 704;
 
 
 	let maze_map;
@@ -18,13 +21,13 @@
 		const width = canvas.width;
 		const height = canvas.height;
 
-		const cellSize = 20;
+		const cellSize = 22;
 		const cols = Math.floor(width / cellSize);
 		const rows = Math.floor(height / cellSize);
 
 
 		const context = canvas.getContext('2d');
-		context.fillStyle = '#ADADAD';
+		context.fillStyle = '#000';
 		context.strokeStyle = '#ccc';
 		context.lineWidth = 1;
 		for (let i = 0; i < rows; i++) {
@@ -44,7 +47,7 @@
 		const width = canvas.width;
 		const height = canvas.height;
 
-		const cellSize = 20;
+		const cellSize = 22;
 		const cols = Math.floor(width / cellSize);
 		const rows = Math.floor(height / cellSize);
 
@@ -52,7 +55,7 @@
 
 		const context = canvas.getContext('2d');
 		
-		context.fillStyle = '#ADADAD';
+		context.fillStyle = '#000';
 		context.strokeStyle = '#ccc';
 		context.lineWidth = 1;
 		for (let i = 0; i < rows; i++) {
@@ -63,6 +66,7 @@
 				context.strokeRect(x, y, cellSize, cellSize);
 			}
 		}
+		generateMaze();
 
 		canvas.addEventListener('mousedown', (event) => {
 		const x = Math.floor(event.offsetX / cellSize);
@@ -110,11 +114,16 @@
 		maze.innerHTML = "{";
 
 		for (let i = 0; i < 32; i++) {
-			maze.innerHTML += "{" + maze_map[i] + "},<br>";
+			if (i == 31){
+				maze.innerHTML += "{" + maze_map[i] + "}}";
+			} else {
+				maze.innerHTML += "{" + maze_map[i] + "},<br>";
+			}
 		}
 	}
 
 </script>
+
 
 <div id="colorButtons">
 	<button on:click={clearCanvas} id="clear">Clear</button>
@@ -122,14 +131,14 @@
 	<button on:click={() => { maze_color = '#000'; maze_number = 0; }} id="black">Black</button>
 	<button on:click={() => { maze_color = '#FFF'; maze_number = 7; }} id="white">White</button>
 	<button on:click={() => { maze_color = '#FF0000'; maze_number = 1; }} id="red">Red</button>
-	<button on:click={() => { maze_color = '#329313'; maze_number = 4; }} id="green">Green</button>
+	<button on:click={() => { maze_color = '#2ECC71'; maze_number = 4; }} id="green">Green</button>
 </div>
 
 <div id="maze_div">
 	<canvas
 		bind:this={canvas}
-		width={640}
-		height={640}
+		width={width}
+		height={height}
 	></canvas>
 
 	<p id="maze"></p>
@@ -140,35 +149,121 @@
 
 
 <style>
+
+	#maze_div {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		justify-items: center;
+	}
+
 	canvas {
-		width: 640px;
-		height: 640px;
+		border: #000 solid;
+		width: 704px;
+		height: 704px;
 		background-color: #666;
 	}
 
+	p{
+		border: #000 solid;
+		word-break: break-all;
+		white-space: normal;
+		text-align: center;
+		font-family: monospace;
+		font-size: large;
+
+		background-color: #ECEFF1;
+		border-radius: 8px;
+		padding: 2em;
+	}
+
+	
+
 	#colorButtons {
-		/*border: #000 solid;*/
-		width: 640px;
-		border-width: 1px;
+		width: 704px;
 		display: flex;
 		align-items: center;
 		justify-content: center;
-		margin-block: 10px;
+		margin-left: 5em;
 	}
 	button {
-		margin-inline: 10px;
-	}
-	#maze_div {
-		border: #666 solid;
-		display: flex;
+		border-radius: 8px;
+		border: #575757 solid;
+		border-width: 0.7px;
+		box-sizing: border-box;
+		color: #FFFFFF;
+		cursor: pointer;
+		display: inline-block;
+		font-family: "Haas Grot Text R Web", "Helvetica Neue", Helvetica, Arial, sans-serif;
+		font-size: 14px;
+		font-weight: 500;
+		height: 40px;
+		line-height: 20px;
+		list-style: none;
+		margin: 10px;
+		outline: none;
+		padding: 10px 16px;
+		position: relative;
+		text-align: center;
+		text-decoration: none;
+		transition: color 100ms;
+		vertical-align: baseline;
+		user-select: none;
+		-webkit-user-select: none;
+		touch-action: manipulation;
 	}
 
-	p{
-		word-break: break-all;
-		white-space: normal;
-		text-align:center;
-		border: #000 solid;
-		width: 500px;
-		margin: 10px;
+	#clear {
+		background-color: #ccc;
 	}
+	#clear:hover,
+	#clear:focus {
+		background-color: #F3F2F2;
+	}
+
+	#black {
+		background-color: #000;
+	}
+	#black:hover,
+	#black:focus {
+		background-color: #90A4AE;
+	}
+
+	#blue {
+		background-color: #201FFF;
+	}
+	#blue:hover,
+	#blue:focus {
+		background-color: #2196F3;
+	}
+
+	#green {
+		background-color: #2ECC71;
+	}
+	#green:hover,
+	#green:focus {
+		background-color: #82E0AA;
+	}
+
+	#red {
+		background-color: #FF0000;
+	}
+	#red:hover,
+	#red:focus {
+		background-color: #FF6363;
+	}
+
+	#white {
+		background-color: #FFF;
+		color: #000;
+	}
+	#white:hover,
+	#white:focus {
+		background-color: #F3F2F2;
+	}
+
+
+
+	
+
+	
 </style>
